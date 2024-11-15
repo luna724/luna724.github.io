38362efaeb1768d13e12e266b5321cf5b86998a9
luna_ <111692896+luna724@users.noreply.github.com>
2024-11-15 17:17:36 +0900

    test on cloud

---
+++ header.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+fetch("/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+    });
+});
